9d24bfeba107dc94ce8462bafe3865c35b03e278
Your Name <lamisazamzam@gmail.com>
2021-04-17 23:43:08 +0600

    Almost Done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const ObjectID = require("mongodb").ObjectID;
+const ObjectId = require("mongodb").ObjectID;
 const port = 5000;
 require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
@@ -16,45 +16,35 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-console.log(process.env.DB_NAME);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
-
 client.connect((err) => {
     const visaCollection = client.db(process.env.DB_NAME).collection("visas");
     const reviewCollection = client
         .db(process.env.DB_NAME)
         .collection("reviews");
     const adminCollection = client.db(process.env.DB_NAME).collection("admins");
+    const orderCollection = client.db(process.env.DB_NAME).collection("orders");
     console.log("db connected");
-    console.log(uri);
 
     app.post("/addService", (req, res) => {
-        console.log(req.body);
         const { detail, price, serviceName, imageURL } = req.body;
         const visaService = { detail, price, serviceName, imageURL };
         visaCollection.insertOne(visaService).then((result) => {
-            console.log(result.insertedCount);
             res.send(result.insertedCount > 0);
         });
     });
 
     app.post("/giveReview", (req, res) => {
-        console.log(req.body);
         const { name, profession, feedback, imgURL } = req.body;
         const review = { name, profession, feedback, imgURL };
         reviewCollection.insertOne(review).then((result) => {
-            console.log(result.insertedCount);
             res.send(result.insertedCount > 0);
         });
     });
 
     app.post("/makeAdmin", (req, res) => {
-        console.log(req.body);
         const { email } = req.body;
         const admin = { email };
         adminCollection.insertOne(admin).then((result) => {
-            console.log(result.insertedCount);
             res.send(result.insertedCount > 0);
         });
     });
@@ -73,10 +63,71 @@ client.connect((err) => {
 
     app.get("/checkIfAdmin", (req, res) => {
         const email = req.query.email;
-        console.log(email);
+
         adminCollection.find({ email: email }).toArray((err, documents) => {
-            console.log(documents);
             res.send(documents);
+        });
+    });
+
+    app.get("/booking/:id", (req, res) => {
+        const id = ObjectId(req.params.id);
+        visaCollection.find({ _id: id }).toArray((err, documents) => {
+            res.send(documents);
+        });
+    });
+
+    app.get("/service/:serviceName", (req, res) => {
+        const serviceName = req.params.serviceName;
+        visaCollection
+            .find({ serviceName: serviceName })
+            .toArray((err, documents) => {
+                res.send(documents);
+            });
+    });
+
+    app.get("/bookings/:email", (req, res) => {
+        const email = req.params.email;
+        adminCollection.find({ email: email }).toArray((err, documents) => {
+            if (documents[0]) {
+                orderCollection.find({}).toArray((err, documents) => {
+                    res.send(documents);
+                });
+            } else {
+                orderCollection
+                    .find({ email: email })
+                    .toArray((err, documents) => {
+                        res.send(documents);
+                    });
+            }
+        });
+    });
+
+    app.post("/placeOrder", (req, res) => {
+        const orderDetail = req.body;
+        orderCollection.insertOne(orderDetail).then((result) => {
+            res.send(result.insertedCount > 0);
+        });
+    });
+
+    app.patch("/updateOrder/:id", (req, res) => {
+        orderCollection
+            .updateOne(
+                { _id: ObjectId(req.params.id) },
+                {
+                    $set: { status: req.body.status },
+                }
+            )
+            .then((result) => {
+                res.send(result.modifiedCount > 0);
+                console.log(result);
+            });
+    });
+
+    app.delete("/deleteService/:_id", (req, res) => {
+        const _id = ObjectId(req.params._id);
+        console.log(_id);
+        visaCollection.deleteOne({ _id: _id }).then((result) => {
+            res.send(result.insertedCount > 0);
         });
     });
 });

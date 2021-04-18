fb3d2b702d1c875718800c7f102dc2dd34d10ba0
Your Name <lamisazamzam@gmail.com>
2021-04-18 12:49:48 +0600

    Done

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const client = new MongoClient(uri, {
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
 
 client.connect((err) => {
     const visaCollection = client.db(process.env.DB_NAME).collection("visas");
